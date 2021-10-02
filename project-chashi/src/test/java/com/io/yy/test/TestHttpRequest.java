ec4af2493ac4afeb09e55bf0d4cbaeb548a27f7c
kris <krazy.chen@gmail.com>
2021-10-03 00:26:45 +0800

    更新接口

--- project-chashi/src/test/java/com/io/yy/test/TestHttpRequest.java
+++ project-chashi/src/test/java/com/io/yy/test/TestHttpRequest.java
@@ -1,6 +1,23 @@
 package com.io.yy.test;
 
 
+import okhttp3.*;
+
+import java.io.IOException;
+
 public class TestHttpRequest {
 
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "clientId=a2a4925348c84f55921331391bf9f800&accessToken=13373d03617e6de7e73d0774cadb2d75&lockId=3731369&pageNo=1&pageSize=100&date=1633191753746");
+        Request request = new Request.Builder()
+                .url("https://api.ttlock.com/v3/lock/listKeyboardPwd")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
 }
