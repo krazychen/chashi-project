6fedd07876c3886b3890d8d0e654eb7eab19b72d
kris <krazy.chen@gmail.com>
2021-12-14 14:35:16 +0800

    refund

--- project-chashi/src/main/java/com/io/yy/wxops/controller/WeixinController.java
+++ project-chashi/src/main/java/com/io/yy/wxops/controller/WeixinController.java
@@ -932,6 +932,7 @@ public class WeixinController extends WeixinSupport {
 
         String return_code = null;
         //判断订单是余额支付 1 还是微信支付 2, 如果是微信支付，则调用微信退款，否则就直接进行反向操作
+        String outRefundNo = "";
         if(csMerchantOrder.getPaymentType()!=null && csMerchantOrder.getPaymentType().equals(2)){
             // 获取微信配置
             List<SysConfigDataRedisVo> sysConfigDataList = ConfigDataUtil.getAllSysConfigData();
@@ -944,7 +945,7 @@ public class WeixinController extends WeixinSupport {
             String SSLCERT_PATH = whyySystemProperties.getRefundUrl();
             String refund_ssl_password = sysConfigDataList.stream().filter(item -> item.getConfigKey().equals("refund_ssl_password")).collect(Collectors.toList()).get(0).getConfigValue();
 
-            String outRefundNo = "reorder_"+UUIDUtil.getUUIDBits(24);
+            outRefundNo = "reorder_"+UUIDUtil.getUUIDBits(24);
             csMerchantOrder.setOutRefundNo(outRefundNo);
 
             Double moneyFen = DoubleUtils.multiply(csMerchantOrder.getOrderPrice(),Double.valueOf("100"));
@@ -959,7 +960,7 @@ public class WeixinController extends WeixinSupport {
 //            packageParams.put("sub_mch_id", sub_mch_id);
             packageParams.put("nonce_str", nonce_str);
             packageParams.put("out_trade_no", csMerchantOrder.getOutTradeNo());//商户订单号
-            packageParams.put("out_refund_no", StringUtils.getRandomStringByLength(32));//商户退款单号
+            packageParams.put("out_refund_no", csMerchantOrder.getOutRefundNo());//商户退款单号
             packageParams.put("total_fee", money);//支付金额，这边需要转成字符串类型，否则后面的签名会失败
             packageParams.put("refund_fee", money);//申请退款金额
             packageParams.put("refund_desc", "申请退款");
@@ -981,7 +982,7 @@ public class WeixinController extends WeixinSupport {
                     + "<out_trade_no>" + csMerchantOrder.getOutTradeNo() + "</out_trade_no>"
                     + "<refund_fee>" + money + "</refund_fee>"
                     + "<total_fee>" + money + "</total_fee>"
-                    + "<refund_desc>" + "申请退款" + "</refund_fee>"
+                    + "<refund_desc>" + "申请退款" + "</refund_desc>"
                     + "<sign>" + mysign + "</sign>"
                     + "</xml>";
 
--- project-chashi/src/test/java/com/io/yy/test/TestApp.java
+++ project-chashi/src/test/java/com/io/yy/test/TestApp.java
@@ -3,7 +3,11 @@ package com.io.yy.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.io.yy.common.api.ApiResult;
 import com.io.yy.util.lang.DateUtils;
+import com.io.yy.util.lang.StringUtils;
+import com.io.yy.wxops.utils.ClientCustomSSL;
+import com.io.yy.wxops.utils.PayUtil;
 import okhttp3.*;
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.text.StringEscapeUtils;
@@ -11,10 +15,7 @@ import org.apache.commons.text.StringEscapeUtils;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestApp {
 
@@ -127,21 +128,87 @@ public class TestApp {
 //        String t2=testS.substring(testS.lastIndexOf("[")+1);
 //
 //        System.out.println(t1+":"+t2);
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url("http://test.520178.net/radio.php?sid=2&ProKey=a1aQWO3O1fv&sname=861714055496545")
-                .method("GET", null)
-                .build();
-        Response response = client.newCall(request).execute();
-        String responseBody = response.body().string();
-        JSONObject jsonObject = JSON.parseObject(responseBody);
-        String Success = jsonObject.getString("Success");
-        if("true".equals(Success)){
-            System.out.println(": 声音提醒正常");
-        }else{
-            System.out.println("声音提醒api存在问题，请检查！");
+//        OkHttpClient client = new OkHttpClient().newBuilder()
+//                .build();
+//        Request request = new Request.Builder()
+//                .url("http://test.520178.net/radio.php?sid=2&ProKey=a1aQWO3O1fv&sname=861714055496545")
+//                .method("GET", null)
+//                .build();
+//        Response response = client.newCall(request).execute();
+//        String responseBody = response.body().string();
+//        JSONObject jsonObject = JSON.parseObject(responseBody);
+//        String Success = jsonObject.getString("Success");
+//        if("true".equals(Success)){
+//            System.out.println(": 声音提醒正常");
+//        }else{
+//            System.out.println("声音提醒api存在问题，请检查！");
+//        }
+
+        try {
+            test2();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
+
+    public static void test2() throws Exception {
+        Map<String, String> packageParams = new HashMap<String, String>();
+
+        packageParams.put("appid", "wxd7f07fdf11d9e7f4");
+        packageParams.put("mch_id", "10031610");
+
+        String s = UUID.randomUUID().toString().replace("-", "");
+        packageParams.put("nonce_str",s);
+
+        String s1 = UUID.randomUUID().toString().replace("-", "");
+        packageParams.put("out_refund_no",s1);
+        packageParams.put("out_trade_no","order_8Xnt9EIhTCCLNdf6oOPUkg**");
+
+        packageParams.put("refund_fee","1");
+        packageParams.put("total_fee","1");
+
+        // 除去数组中的空值和签名参数
+        packageParams = PayUtil.paraFilter(packageParams);
+        String prestr = PayUtil.createLinkString(packageParams); // 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+
+        //MD5运算生成签名，这里是第一次签名，用于调用统一下单接口
+        String sign = PayUtil.sign(prestr, "Sxj5566Sxj5566Sxj5566Sxj5566Sxj5", "utf-8").toUpperCase();
+
+        packageParams.put("sign", sign);
+
+        //拼接统一下单接口使用的xml数据，要将上一步生成的签名一起拼接进去
+        String xml = "<xml>" + "<appid>" + "wxd7f07fdf11d9e7f4" + "</appid>"
+                + "<mch_id>" + "10031610" + "</mch_id>"
+//                    + "<sub_mch_id>" + sub_mch_id + "</sub_mch_id>"
+                + "<nonce_str>" + s + "</nonce_str>"
+                + "<out_refund_no>" + s1 + "</out_refund_no>"
+                + "<out_trade_no>" + "order_8Xnt9EIhTCCLNdf6oOPUkg**" + "</out_trade_no>"
+                + "<refund_fee>" + "1" + "</refund_fee>"
+                + "<total_fee>" + "1" + "</total_fee>"
+//                + "<refund_desc>" + "申请退款" + "</refund_fee>"
+                + "<sign>" + sign + "</sign>"
+                + "</xml>";
+
+        System.out.println("调试模式_退款接口 请求XML数据：" + xml);
+
+        //调用统一下单接口，并接受返回的结果
+//            String result = PayUtil.httpRequest(refuse_url, "POST", xml);
+        String result = ClientCustomSSL.doRefund("https://api.mch.weixin.qq.com/secapi/pay/refund",xml,"E:\\2019other\\chashi\\apiclient_cert.p12","10031610");
+
+        System.out.println("调试模式_退款接口 返回XML数据：" + result);
+
+        // 将解析结果存储在HashMap中
+        Map map = PayUtil.doXMLParse(result);
+
+        String return_code = (String) map.get("return_code");//返回状态码
+
+        if (!StringUtils.equals(return_code, "SUCCESS")){
+            System.out.println((String) map.get("return_msg"));
+        }
+
+
+
+    }
+
 }
